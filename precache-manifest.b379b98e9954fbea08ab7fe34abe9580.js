c57e48c54f81fee0162a58af98cae679be059b40
Code Sandbox <hello@codesandbox.io>
2022-03-27 04:50:07 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b379b98e9954fbea08ab7fe34abe9580.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e98c91ebb4ff9a9a5a1f",
+    "url": "https://3egg.github.io/csb-bfy3sv/static/js/main.2505ae17.chunk.js"
+  },
+  {
+    "revision": "9b611964a055e2c5b825",
+    "url": "https://3egg.github.io/csb-bfy3sv/static/js/runtime~main.710a65cf.js"
+  },
+  {
+    "revision": "3a5f5c83656570ebd845",
+    "url": "https://3egg.github.io/csb-bfy3sv/static/js/2.076216f4.chunk.js"
+  },
+  {
+    "revision": "35c60b42e0f1d54a03ded36d652faf27",
+    "url": "https://3egg.github.io/csb-bfy3sv/index.html"
+  }
+];
